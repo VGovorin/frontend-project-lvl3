23dfe4171ac674567bb0e5c4098b70e7de694ddf
Владислав Говорин <vvgovorin@gmail.com>
2022-03-01 21:57:09 +0800

    update getData (remove fn proxy)

--- src/getData.js
+++ src/getData.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const proxy = (url) => {
-  const href = new URL('https://hexlet-allorigins.herokuapp.com/get?disableCache=true');
-  href.searchParams.set('url', url);
-  return href.toString();
-};
+// const proxy = (url) => {
+//   const href = new URL('https://hexlet-allorigins.herokuapp.com/get?disableCache=true');
+//   href.searchParams.set('url', url);
+//   return href.toString();
+// };
 
-export default (url) => axios.get(proxy(url))
+export default (url) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(`${url}`)}&disableCache=true)`)
   .catch(() => {
     const error = new Error();
     error.networkError = true;
